17ef552ce3792b8de3ddbd6340bd1fc1bcc98b79
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-03-31 07:48:56 -0400

    Spell check completed

--- save_tum_file.py
+++ save_tum_file.py
@@ -1,4 +1,4 @@
-"""Code to save quarternion data to a txt file
+"""Code to save quaternion data to a txt file
 
 Attributes:
     ser (TYPE): Serial
@@ -37,10 +37,10 @@ time_stamps_rpy = []
 
 time_start = time.time()
 
-f = open(test_name +"quarternion_data.txt", "w+")
+f = open(test_name +"quaternion_data.txt", "w+")
 
 
-def euler_from_quaternion(quarternion_list):
+def euler_from_quaternion(quaternion_list):
     """
     Convert a quaternion into euler angles (roll, pitch, yaw)
     roll is rotation around x in radians (counterclockwise)
@@ -48,10 +48,10 @@ def euler_from_quaternion(quarternion_list):
     yaw is rotation around z in radians (counterclockwise)
     """
 
-    x = float(quarternion_list[0])
-    y = float(quarternion_list[1])
-    z = float(quarternion_list[2])
-    w = float(quarternion_list[3])
+    x = float(quaternion_list[0])
+    y = float(quaternion_list[1])
+    z = float(quaternion_list[2])
+    w = float(quaternion_list[3])
 
     t0 = +2.0 * (w * x + y * z)
     t1 = +1.0 - 2.0 * (x * x + y * y)
