ff4232b51890172cc7b921147e92e4839661b7a8
Govind Ajith Kumar <50176400+govindak-umd@users.noreply.github.com>
2021-03-31 10:28:22 -0400

    Code cleanup

--- capture_video.py
+++ capture_video.py
@@ -8,20 +8,20 @@ videoWriter = cv2.VideoWriter(test_name + 'video' +'.avi', fourcc, 20.0, (640,48
 print('Capturing Video now ...')
 
 try:
-	while (True):
-		ret, frame = capture.read()
-		 
-		if ret:
-		    cv2.imshow('video', frame)
-		    videoWriter.write(frame)
+    while (True):
+        ret, frame = capture.read()
 
-		if cv2.waitKey(1) == 27:
-		    break
-	 
-	capture.release()
-	videoWriter.release()
-	 
-	cv2.destroyAllWindows()
+        if ret:
+            cv2.imshow('video', frame)
+            videoWriter.write(frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+    capture.release()
+    videoWriter.release()
+
+    cv2.destroyAllWindows()
 
 # Save when the user exits
 except KeyboardInterrupt:
